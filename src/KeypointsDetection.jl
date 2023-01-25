422ebad36988a80af4cab460e55beba36f0f2604
gortibaldik <ferotre@gmail.com>
2023-01-25 16:59:42 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.30

---
+++ src/KeypointsDetection.jl
@@ -0,0 +1,5 @@
+module KeypointsDetection
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using KeypointsDetection
+using Test
+
+@testset "KeypointsDetection.jl" begin
+    # Write your tests here.
+end
