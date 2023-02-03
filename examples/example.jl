fcbb8688733b2de3e6ebc0597a212ee6e7354c61
gortibaldik <ferotre@gmail.com>
2023-02-03 16:41:26 +0100

    Update examples

--- examples/example.jl
+++ examples/example.jl
@@ -2,11 +2,13 @@ module VideoExample
 using Revise
 using KeypointsDetection
 
-export yolo
+trainDataframe = KeypointsDetection.load_train_dataframe();
+X, y = KeypointsDetection.create_train_dataset_full(trainDataframe);
+net = KeypointsDetection.define_net_simple_feedforward()
+train_losses_steps, valid_losses = KeypointsDetection.train_gpu_net!(net, X, y);
+KeypointsDetection.plot_losses(train_losses_steps, valid_losses)
+predictedDataframe = KeypointsDetection.predict_to_dataframe(net, trainDataframe);
+KeypointsDetection.show_image(predictedDataframe, 1)
 
-function yolo()
-    println("Hello")
-    KeypointsDetection.play_test_video()
-end
 
 end # VideoExample
